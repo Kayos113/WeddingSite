c283401d5f80e8870d1ea0c03331629d5503541e
William Knapp <willyknapp@gmail.com>
2022-07-11 14:12:06 -0500

    Blank Slate for next website segment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import Header from "./components/Header"
+import Information from "./components/Information"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Information />
     </div>
   );
 }
---
+++ src/components/Information.js
@@ -0,0 +1,11 @@
+import "./css/Information.css"
+
+function Information() {
+
+  return (
+    <div className="information">
+    </div>
+  )
+}
+
+export default Information;
