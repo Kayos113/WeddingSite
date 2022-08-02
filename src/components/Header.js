cea11e20e9fe7205a1c7ea27bd842989124aa178
William Knapp <willyknapp@gmail.com>
2022-08-02 15:25:26 -0400

    images added, header background image set

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import "./css/Header.css";
+
+function Header() {
+  return (
+    <div className="Header-Component">
+      <div className="header">
+      </div>
+    </div>);
+}
+
+export default Header;
