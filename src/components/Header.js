9050397805883c2e7b09554a37485c6143d0d2fc
William Knapp <willyknapp@gmail.com>
2022-08-02 15:49:23 -0400

    Text card added to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,11 @@ function Header() {
   return (
     <div className="Header-Component">
       <div className="header">
+        <div className="text-card">
+          <h1>Natalie and William</h1>
+          <p className="subscript">Save the Date:</p>
+          <p>September 16, 2023</p>
+        </div>
       </div>
     </div>);
 }
