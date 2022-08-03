269ee4f312d55ca3a3709e7d2bbb7bbc65d6e7d2
Will <willyknapp@gmail.com>
2022-08-02 20:48:09 -0400

    header text-card complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,22 @@
 import "./css/Header.css";
+import floral from "../images/floral-vector-cream.svg";
 
 function Header() {
   return (
     <div className="Header-Component">
       <div className="header">
         <div className="text-card">
+          <img src={floral} alt=""/>
           <h1>Natalie and William</h1>
-          <p className="subscript">Save the Date:</p>
-          <p>September 16, 2023</p>
+          <div className="save-date">
+            <div className="date">
+              <p className="subscript">Save the Date:</p>
+              <p>September 16, 2023</p>
+            </div>
+            <div className="rsvp">
+              <h3>RSVP ↓</h3>
+            </div>
+          </div>
         </div>
       </div>
     </div>);
