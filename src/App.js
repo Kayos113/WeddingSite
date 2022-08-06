7a9d692ca20b1705dab2cc642445db3da29ab2c6
Will <willyknapp@gmail.com>
2022-08-06 17:15:33 -0400

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import Header from "./components/Header"
 import Information from "./components/Information"
+import Transition from "./components/Transition"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Transition />
       <Information />
     </div>
   );
---
+++ src/components/Transition.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./css/Transition.css";
+
+function Transition() {
+  return (
+    <div className="maroon-to-cream">
+    </div>
+  )
+}
+
+export default Transition;
