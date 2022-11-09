3ae1bd653886c08c68b1dc81b8dcaf091fc8d1d6
William Knapp <willyknapp@gmail.com>
2022-11-09 15:25:55 -0500

    new header picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Banner from "./Banner.js";
 import Countdown from "./Countdown.js";
 import "./css/Header.css";
 // import background from "./images/court_gaze_close_4898x3265.jpg";
-import background from "./images/engagement/lake_gaze.jpg"
+import background from "./images/engagement/lake_boop.jpg"
 
 function Header() {
 
