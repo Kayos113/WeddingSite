03cac184ec82f575a3ad7d2a9fc95ad3735252ce
William Knapp <willyknapp@gmail.com>
2022-11-09 14:56:25 -0500

    Changed picture layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Banner from "./Banner.js";
 import Countdown from "./Countdown.js";
 import "./css/Header.css";
 // import background from "./images/court_gaze_close_4898x3265.jpg";
-import background from "./images/engagement/b_w_grass.jpg"
+import background from "./images/engagement/lake_gaze.jpg"
 
 function Header() {
 
