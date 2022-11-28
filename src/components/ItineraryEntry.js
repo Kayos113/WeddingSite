5c6a96606106a54877693ec31381ae936853f789
William Knapp <willyknapp@gmail.com>
2022-11-28 12:15:22 -0500

    Flesh out the itinerary as a rough draft to be finialized with Natalie.
    I'm not in love with the design of the Entries tehmselves so that may change, but it is close enough to what will be the final product.

--- src/components/ItineraryEntry.js
+++ src/components/ItineraryEntry.js
@@ -3,11 +3,9 @@ import React from "react";
 function ItineraryEntry(props) {
   return (
     <div className="entry-container">
-      <div className="time-container">
-        <div/>
+      <div className="column-container">
         <p>{props.leftCol}</p>
         <p>{props.rightCol}</p>
-        <div/>
       </div>
       {props.caption===true?null:<p className="caption">{props.caption}</p>}
     </div>
