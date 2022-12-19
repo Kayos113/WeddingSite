41dbd242307b3e7ac12f9d4276157871693de574
William Knapp <willyknapp@gmail.com>
2022-12-19 14:55:53 -0500

    misses Interest Form update with RadioQuestion commit. Adding to new commit here.

--- src/components/InterestForm.js
+++ src/components/InterestForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import RadioQuestion from "./RadioQuestion";
 import "./css/InterestForm.css";
 
 function InterestForm() {
@@ -14,44 +15,44 @@ function InterestForm() {
     event.preventDefault();
   }
 
+  const arrivalOptions = [
+    {id: "friday",
+    value:"Friday"},
+    {id: "saturday",
+    value:"Saturday"}
+  ];
+
+  const plusOneOptions = [
+    {id: "no-plus",
+    value:"No Plus One"},
+    {id: "plus-one",
+    value:"Plus One"},
+    {id:"plus-more",
+    value:"Plus More than One"}
+  ];
+
   return (
     <div className="tab-container">
       <article className="interest-form">
         <form onSubmit={onSubmit}>
-          <h4 classNAme="headline">I'll be there!</h4>
+        
+          <h4 className="headline">I'll be there!</h4>
           <p className="tagline">(Not an RSVP)</p>
           <input type="text" name="name" placeholder="Guest Name" className="textfield" onInput={textInput}/>
-          <div className="radio-question">
-            <p className="headline">Do you think you'll arrive Friday or Saturday?</p>
-            <div className="radio-container">
-              <div className="single-radio">
-                <input type="radio" name="arrival" id="friday" value="Friday-Sunday"/>
-                <label for="friday">Friday Arrival</label>
-              </div>
-              <div className="single-radio">
-                <input type="radio" name="arrival" id="saturday" value="Saturday-Sunday"/>
-                <label for="saturday">Saturday Arrival</label>
-              </div>
-            </div>
-          </div>
-          // <div className="radio-question">
-          //   <p className="headline">Will you be bringing a Plus One?</p>
-          //   <p className="tagline">(Third option is for poly friends)</p>
-          //   <div className="radio-container">
-          //     <div className="single-radio">
-          //       <input type="radio" name="plus-one" id="no-plus" value="No Plus One"/>
-          //       <label for="no-plus">No Plus One</label>
-          //     </div>
-          //     <div className="single-radio">
-          //       <input type="radio" name="plus-one" id="plus-one" value="Plus One"/>
-          //       <label for="plus-one">Plus One</label>
-          //     </div>
-          //     <div className="single-radio">
-          //       <input type="radio" name="plus-one" id="plus-more" value="Plus More than One"/>
-          //       <label for="plus-more">Plus More than One</label>
-          //     </div>
-          //   </div>
-          // </div>
+
+          <RadioQuestion
+          question="Do you think you'll arrive Friday or Saturday?"
+          name="arrival"
+          options={arrivalOptions}
+          />
+
+          <RadioQuestion
+            question="Will you be bringing a Plus One"
+            tagline="(Third option is for poly friends)"
+            name="plus-one"
+            options={plusOneOptions}
+          />
+
         </form>
       </article>
     </div>
