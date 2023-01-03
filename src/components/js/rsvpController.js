cbeb3c5a8bc2cdae7b6c30833404ee12ae095481
William Knapp <willyknapp@gmail.com>
2023-01-03 14:11:50 -0500

    Update the logic behind the radioInput function, going from static if-else statements to a forEach loop which only updates the value that needs to

--- src/components/js/rsvpController.js
+++ src/components/js/rsvpController.js
@@ -1,46 +1,42 @@
 exports.radioLogic = function(prevValue, newValue, name) {
-  if(name === "attendance") {
-    return [{
-      name:prevValue[0].name,
-      value:newValue
-    },
-    {
-      name:prevValue[1].name,
-      value:prevValue[1].value
-    },
-    {
-      name:prevValue[2].name,
-      value:prevValue[2].value
-    }]
-  }
-  else if (name === "arrival") {
-    return [{
-      name:prevValue[0].name,
-      value:prevValue[0].value
-    },
-    {
-      name:prevValue[1].name,
-      value:newValue
-    },
-    {
-      name:prevValue[2].name,
-      value:prevValue[2].value
-    }]
-  }
-  else if (name === "accomodations") {
-    return [{
-      name:prevValue[0].name,
-      value:prevValue[0].value
-    },
-    {
-      name:prevValue[1].name,
-      value:prevValue[1].value
-    },
-    {
-      name:prevValue[2].name,
-      value:newValue
-    }]
-  }
+  // if(name === "attendance") {
+  //   return [{
+  //     name:prevValue[0].name,
+  //     value:newValue
+  //   },
+  //   {
+  //     name:prevValue[1].name,
+  //     value:prevValue[1].value
+  //   },
+  //   {
+  //     name:prevValue[2].name,
+  //     value:prevValue[2].value
+  //   }]
+  // }
+  // else if (name === "arrival") {
+  //   return [{
+  //     name:prevValue[0].name,
+  //     value:prevValue[0].value
+  //   },
+  //   {
+  //     name:prevValue[1].name,
+  //     value:newValue
+  //   },
+  //   {
+  //     name:prevValue[2].name,
+  //     value:prevValue[2].value
+  //   }]
+  // }
+  // Now that I am adding a third question, it will be easier to build a loop to pass through and check the name of the newValue against the name of the prevValue as the name will stay constant
+  let newRadioArr = [];
+  prevValue.forEach( (response, index) => {
+    if(prevValue[index].name === name) { // this is the radio selection that was updated
+      newRadioArr.push({name: prevValue[index].name, value: newValue})
+    } else { // this is not the response that was updated and should remain the same
+      newRadioArr.push({name: prevValue[index].name, value: prevValue[index].value});
+    }
+  });
+  return newRadioArr;
 };
 
 exports.textfieldLogic = function(prevValue, newValue, id) {
