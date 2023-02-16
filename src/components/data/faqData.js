39af8d4dee4e67c19216b3331ab8e9b4beee6815
Will <willyknapp@gmail.com>
2023-02-16 16:31:19 -0500

    faq questions added

---
+++ src/components/data/faqData.js
@@ -0,0 +1,58 @@
+exports.faqArr = [
+  {
+    question:"How Do I RSVP?",
+    answer:"The RSVP section isn’t live yet but once it is, please RSVP online here by June 1st."
+  },
+  {
+    question:"Can I bring a plus one?",
+    answer:"Unless their name is on the invitation, please reach out to us. Since on-site accommodations are limited, you likely will be responsible for finding nearby accommodations for both you and your +1."
+  },
+  {
+    question:"Are kids allowed?",
+    answer:"Aside from specifically-invited family members, we ask that kids stay at home."
+  },
+  {
+    question:"What is the dress code for the wedding?",
+    answer:"For the ceremony and reception, we ask that you dress in formal, black-tie optional attire. We will be outside for the ceremony and cocktail hour, indoors for the reception and back out for the bonfire. Feel to dress how you like for the rest of the weekend. There will be hot tubs and a heated pool so pack a swimsuit!"
+  },
+  {
+    question:"Will I need to pre-select my reception dinner meal?",
+    answer:"No, we will be serving food family style. However, please do let us know if you have dietary restrictions."
+  },
+  {
+    question:"Which meals will be provided for the weekend?",
+    answer:"Immediate family members and the wedding party will be invited to the rehearsal dinner. Otherwise, dinner will be provided at the reception along with a next-day brunch buffet for all on Sunday. Please frequent one of the local restaurants or bring food to prepare in one of the on-site kitchens for other meals."
+  },
+  {
+    question:"Is there an open bar?",
+    answer:"Yes!"
+  },
+  {
+    question:"Will I be staying on-site at Estates By Brophy?",
+    answer:"Space at the Estates is limited. The Riverview Estate houses 32 people while the Deer Ridge Estate sleeps 40. Because of this, priority will be given to 1. The wedding party & family and 2. People who plan on staying the full weekend (Sept 15-Sept 17).\nThose not staying on-site will be able to stay in a local hotel or Airbnb. We have hotel blocks at some nearby resorts, or you’re welcome to find your own accommodations."
+  },
+  {
+    question:"When should I book my accomodations?",
+    answer:"If you know you are unable to stay for the full weekend or prefer to secure your own lodgings, you can book as soon as you like. Please refer to our list of local hotels and Airbnbs here./nWe will be booking the rooms for those staying on-site./nOtherwise, please wait until the invites are sent out and the RSVP form is up. This should be around mid-April."
+  },
+  {
+    question:"How can I get to Fishs Eddy, NY?",
+    answer:"Fishs Eddy, NY is approximately 2.5 hours outside of NYC. There is on-site parking for those who choose to drive. If you are local to NY but driving or renting a car isn’t an option for you, there are train and bus options through Amtrak, VIA Rail, Metro-North Railroad and Trailways. For those coming from out of state, Albany is the closest airport but there are other options that provide buses directly to the area."
+  },
+  {
+    question:"How do I sign up to perform in the welcome night cabaret?",
+    answer:"Everyone is welcome to perform in the cabaret but obviously, no one is required to! If you have a song to sing; a monologue, poem or stand-up set to say or any other unique talent to showcase, you can sign up during check-in hours at Riverview Estate by Brophy. The cabaret will take place on Friday, Sept 15 at 7pm."
+  },
+  {
+    question:"Is Natalie changing her last name?",
+    answer:"No. So, please refrain from addressing any gifts or cards to \"Mrs. Knapp.\""
+  },
+  {
+    question:"Will there be alpacas at the wedding?",
+    answer:"Duh."
+  },
+  {
+    question:"Congratulations!",
+    answer:"Thank you! That's not really a question, but good hustle."
+  }
+]
--- src/components/faqTab.js
+++ src/components/faqTab.js
@@ -1,22 +1,34 @@
 import React from "react";
 import FAQEntry from "./faq";
+import { faqArr } from "./data/faqData.js";
 import "./css/faq.css";
 
 function faqTab() {
+
+  function faqElems() {
+    let entries = [];
+    faqArr.forEach( (entry, index) => {
+      entries.push(<FAQEntry question={entry.question} answer={entry.answer} key={"entry"+index} />)
+    });
+    return entries;
+  }
+
   return (<div className="tab-container">
       <article>
         <h1>FAQ</h1>
 
-
-        <FAQEntry
-          question="Should we book our own rooms at your venue?"
-          answer="No. Our venue does have limited space for accomodations which is going to be coordinated by us and the venue to fit as many quests as possible with a preference given to family, and the wedding party. There are also be nearby hotels with blocks of rooms to reserve coordinated by the wedding, or numerous nice AirBnB, Vrbo and other lodgings if you are inclined to find your own."
-        />
-
-        <FAQEntry
-          question="Congratulations!"
-          answer="Thank you! That's not really a question, but good hustle."
-        />
+        {faqElems()}
+        {
+        //   <FAQEntry
+        //   question="Should we book our own rooms at your venue?"
+        //   answer="No. Our venue does have limited space for accomodations which is going to be coordinated by us and the venue to fit as many quests as possible with a preference given to family, and the wedding party. There are also be nearby hotels with blocks of rooms to reserve coordinated by the wedding, or numerous nice AirBnB, Vrbo and other lodgings if you are inclined to find your own."
+        // />
+        //
+        // <FAQEntry
+        //   question="Congratulations!"
+        //   answer="Thank you! That's not really a question, but good hustle."
+        // />
+      }
 
       </article>
     </div>);
